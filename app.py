c040a7b371931ce9bb129e6e94e568ba512478ab
Kritik Modi <kritik.modi@juspay.in>
2023-06-26 23:24:30 +0530

    remove get method

--- app.py
+++ app.py
@@ -2,12 +2,10 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/', methods=['POST'])
 def index():
-    if request.method == 'POST':
-        text = request.form['text']
-        return render_template('results.html', text=text)
-    return render_template('index.html')
+    text = request.form['text']
+    return render_template('results.html', text=text)
 
 if __name__ == '__main__':
     app.run()
