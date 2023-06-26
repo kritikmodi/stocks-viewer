d07961749308f89d1a2ba367d34a18b6c9866acb
Kritik Modi <kritik.modi@juspay.in>
2023-06-27 00:08:16 +0530

    Add changes

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
-from flask import Flask, render_template, request
+from flask import Flask, request
+import yfinance as finance
 
 app = Flask(__name__)
 
 @app.route('/', methods=['POST'])
 def index():
-    text = request.form['text']
-    return render_template('results.html', text=text)
+    company_code = request.form['text']
+    company_ticker = finance.Ticker(company_code)
+    return company_ticker
 
 if __name__ == '__main__':
     app.run()
